dbb288994ae5ab78d11c417323f0e13c18977354
DominikPieprzyk <61658908+DominikPieprzyk@users.noreply.github.com>
2021-06-22 14:52:22 +0200

    Newer Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,71 @@
-import "./App.scss";
-
 import {
+  BrowserRouter as Router,
+  Switch,
   Route,
-  HashRouter as Router,
-  Switch
+  Redirect
 } from "react-router-dom";
+import { useState, useEffect } from 'react';
+import './App.scss';
+import Home from './pages/home/home';
+import PageNotFound from "./pages/404/404";
+import User from "./models/user";
 
-import Home from './components/Home/Home';
-import Menu from './components/Menu/Menu';
-import Navbar from './components/Navbar/Navbar';
-import { Provider } from 'react-redux';
-import React from 'react';
-import configureStore from './store';
+import { getPhotoById } from "./api/photos";
+import { getUser } from "./api/users";
+import Photo from "./models/photo";
 
-const store = configureStore();
-const routes = [
-  {
-    exact: true,
-    path: "/",
-    component: Home
-  },
-  // {
-  //   path: "/profile/:userId",
-  //   component: Profile
-  // },
-  // {
-  //   path: "/workspace/:id",
-  //   component: Workspace
-  // },
-  // {
-  //   path: "/entities",
-  //   component: Entities
-  // },
-  // {
-  //   path: "/*",
-  //   component: NotFound
-  // }
-  // to be implemented
-];
+import NavBar from './components/NavBar/NavBar';
+import LeftSideMenuComponent from './components/left-side-menu/left-side-menu.component';
+import Entities from "./pages/entities/entities";
+import Profile from "./pages/profile/profile";
+import Workspace from "./pages/workspace/workspace";
 
 
 function App() {
+  const userId: number = 1;
+
+  const [user = new User(), setUser] = useState<User>();
+  useEffect(() => {
+    getUser(userId).then(response => { setUser(response.data); });
+  }, [user.id]);
+
+  const [photo = new Photo(), setPhoto] = useState<Photo>();
+  useEffect(() => {
+    getPhotoById(userId).then(response => { setPhoto(response.data) });
+  }, [photo.id]);
 
   return (
-    <Provider store={store}>
+    <div className="App">
       <Router>
-        <div className="app">
-          <Navbar />
-          <div className="app-window">
-            <Menu />
-            <main>
-              <Switch>
-                {routes.map((route, i) => (
-                  <RouteWithSubRoutes key={i} {...route} />
-                ))}
-              </Switch>
-            </main>
-          </div>
+        <NavBar/>
+        <div className="main-app-content">
+          <LeftSideMenuComponent user={user} photo={photo} />
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route exact path="/">
+              <Redirect to="/home" />
+            </Route>
+            <Route path="/home">
+              <Home />
+            </Route>
+            <Route path="/404">
+              <PageNotFound />
+            </Route>
+            <Route path="/entities">
+              <Entities />
+            </Route>
+            <Route path="/profile">
+              <Profile />
+            </Route>
+            <Route path="/workspace/:id">
+              <Workspace />
+            </Route>
+          </Switch>
         </div>
       </Router>
-    </Provider>
+    </div>
   );
 }
 
-function RouteWithSubRoutes(route: typeof routes[0]) {
-  return (
-    <Route
-      exact={route.exact}
-      path={route.path}
-      render={(props: any) => (
-        <route.component {...props} />
-      )}
-    />
-  );
-}
 export default App;
---
+++ src/pages/profile/profile.tsx
@@ -0,0 +1,191 @@
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import FormatAlignJustifyIcon from '@material-ui/icons/FormatAlignJustify';
+import InsertDriveFileIcon from '@material-ui/icons/InsertDriveFile';
+import { useEffect, useState } from 'react';
+import { getPhotoById } from '../../api/photos';
+import { getUser } from '../../api/users';
+import Photo from '../../models/photo';
+import User from '../../models/user';
+import './profile.scss';
+import EditIcon from '@material-ui/icons/Edit';
+import CloseIcon from '@material-ui/icons/Close';
+import AddIcon from '@material-ui/icons/Add';
+import SaveIcon from '@material-ui/icons/Save';
+const Profile = () => {
+    const userId: number = 3;
+    const [user, setUser] = useState<User>();
+    const [userDataEditorEnabled, setUserDataEditor] = useState<boolean>(false);
+    const [userDetailsEnabled, setDetail] = useState<boolean>(false);
+    useEffect(() => {
+        getUser(userId).then(usr => {
+            setUser(usr.data);
+        })
+    }, [user?.id]);
+
+    const [photo = new Photo(), setPhoto] = useState<Photo>();
+    useEffect(() => {
+        getPhotoById(userId).then(response => { setPhoto(response.data) });
+    }, [photo.id]);
+
+    const [expertisesClear, setExpertisesClear] = useState<string[]>([]);
+    const [specialitiesClear, setSpecialitiesClear] = useState<string[]>([]);
+    const [admissionsClear, setAdmissionsClear] = useState<string[]>([]);
+    const [countiesClear, setCountiesClear] = useState<string[]>([]);
+    const [expertises, setExpertises] = useState<string[]>(["Manage and acquisition"]);
+    const [specialities, setSpecialities] = useState<string[]>(["Cross border operation", "Transactions over 500M€/$"]);
+    const [admissions, setAdmissions] = useState<string[]>(["Paris bar association", "Tunisian bar association"]);
+    const [counties, setCounties] = useState<string[]>(["Tunisia"]);    
+
+    const enableEditDetails = (): void => {
+        if (!userDetailsEnabled) {
+            setExpertisesClear([...expertises]);
+            setSpecialitiesClear([...specialities]);
+            setAdmissionsClear([...admissions]);
+            setCountiesClear([...counties]);
+        }
+        else {
+            setExpertises(expertisesClear);
+            setSpecialities(specialitiesClear);
+            setAdmissions(admissionsClear);
+            setCounties(countiesClear);
+        }
+        setDetail(!userDetailsEnabled)
+    }
+
+
+    const saveDetails = (): void => {
+      //TODO: PUT ON SAVE
+      
+      setDetail(!userDetailsEnabled);
+    }
+
+    const handleChangeExpertise = (index: number, value: string) => {
+        var clone = [...expertises];
+        clone[index] = value;
+    }
+
+
+    return (
+        <div className="Profile">
+            <div className="container">
+                <div className="info-row">
+                    <div className="elements"><ChatBubbleOutlineIcon /><span>Message</span></div>
+                    <div className="elements"><FormatAlignJustifyIcon /><span>Create a request</span></div>
+                    <div className="elements"><InsertDriveFileIcon /><span>Add a cluster</span></div>
+                </div>
+                <div className="user-wrapper">
+                    <div className="left-column">
+                        <div>
+                            <img src={photo.thumbnailUrl} alt="thumbnail" />
+                        </div>
+                        <span>
+                            See profile
+                        </span>
+                    </div>
+                    <div className="right-column">
+                        <div className="edit-wrapper">
+                            <button className="edit">
+                                {userDataEditorEnabled ? <SaveIcon /> : null}
+                            </button>
+                            <button className="edit" onClick={() => { setUserDataEditor(!userDataEditorEnabled) }}>
+                                {userDataEditorEnabled ? <CloseIcon /> : <EditIcon />}
+                            </button>
+                        </div>
+                        <input type="text" disabled={!userDataEditorEnabled} value={user?.name} />
+                        <input type="text" disabled={!userDataEditorEnabled} value={user?.username} />
+                        <div>
+                            <div>
+                                <input type="text" disabled={!userDataEditorEnabled} value={user?.company.name} />
+                                <input type="text" disabled={!userDataEditorEnabled} value={user?.website} />
+
+                            </div>
+                            <div>
+                                <select disabled={!userDataEditorEnabled}>
+                                    <option>Contractor</option>
+                                    <option>Partner</option>
+                                </select>
+                                <input type="text" disabled={!userDataEditorEnabled} value={user?.phone} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <hr />
+
+                <div className="details">
+                    <div className="edit-wrapper">
+                        <button className="edit" onClick={() => { saveDetails() }}>
+                            {userDetailsEnabled ? <SaveIcon /> : null}
+                        </button>
+                        <button className="edit" onClick={() => { enableEditDetails() }}>
+                            {userDetailsEnabled ? <CloseIcon /> : <EditIcon />}
+                        </button>
+
+                    </div>
+
+                    <div>
+                        <h2>Expertise</h2>
+                        <div className="details-input-wrapper">
+                            {
+                                expertises.map((e, i) => {
+                                    return (
+                                        <input type="text" value={e}  onChange={(e) => { handleChangeExpertise(i, e.currentTarget.value) }}      key={i} disabled={!userDetailsEnabled} />
+                                    );
+                                })
+                            }
+                            {userDetailsEnabled ? <button onClick={() => setExpertises(expertises.concat(""))}><AddIcon /></button> : ""}
+                        </div>
+                    </div>
+                    <div>
+                        <h2>Specialities</h2>
+                        <div className="details-input-wrapper">
+                            {
+                                specialities.map((e, i) => {
+                                    return (
+                                        <input type="text" value={e} key={i} disabled={!userDetailsEnabled} />
+                                    );
+                                })
+                            }
+                            {userDetailsEnabled ? <button onClick={() => { setSpecialities(specialities.concat("")) }}><AddIcon /></button> : ""}
+                        </div>
+                    </div>
+
+                    <div>
+                        <h2>Admissions</h2>
+                        <div className="details-input-wrapper">
+                            {
+                                admissions.map((e, i) => {
+                                    return (
+                                        <input type="text" value={e} readOnly key={i} disabled={!userDetailsEnabled} />
+                                    );
+                                })
+
+                            }
+                            {userDetailsEnabled ? <button onClick={() => setAdmissions(admissions.concat("")) }><AddIcon /></button> : ""}
+                        </div>
+                    </div>
+
+                    <div>
+                        <h2>Counties</h2>
+                        <div className="details-input-wrapper">
+                            {
+                                counties.map((e, i) => {
+                                    return (
+                                        <input type="text" value={e} readOnly key={i} disabled={!userDetailsEnabled} />
+                                    );
+                                })
+                            }
+                            {userDetailsEnabled ? <button onClick={() => setCounties(counties.concat(""))}><AddIcon /> </button> : ""}
+                        </div>
+                    </div>
+
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Profile;
